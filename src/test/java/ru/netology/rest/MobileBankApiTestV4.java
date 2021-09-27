037bc72be636a190b95aaeb054d4c4322dc49ce2
iqaenganer <daw.camalian@yandex.ru>
2021-09-27 15:48:15 +0300

    Добавление appveyor.yml

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -1,11 +1,11 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV4 {
     @Test
@@ -21,7 +21,11 @@ class MobileBankApiTestV4 {
                 .then()
                 .statusCode(200)
                 .body("", hasSize(3))
-                .body("[0].currency",equalTo("RUB"))
+                .body("[0].balance", greaterThanOrEqualTo(0))
+                .body("[0].currency", equalTo("RUB"))
+                .body("[0].name", equalTo("Текущий счёт"))
+                .header("Content-Type","application/json; charset=UTF-8")
+                .contentType(ContentType.JSON)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
 
         ;
